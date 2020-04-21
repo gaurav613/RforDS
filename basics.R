7333c07a1bb5e0d0f239b8ccb1875544a7eb39a4
gaurav613 <gigs612@gmail.com>
2020-04-21 17:25:55 -0400

    data wrangling & plotting

--- basics.R
+++ basics.R
@@ -40,7 +40,40 @@ sort(murders$total)# sorts in increasing order
 index <- order(murders$total)# gives indices in increasing order of values
 murders$state[index]
 rank(murders$total)# gives the rank of values(smallest to biggest)
-
+# rank(-x) gives you the ranks from highest to lowest.
 # vector arithmetic
 murder_rate <- murders$total/murders$population # can operate on complete vectors
 murders$state[order(murder_rate,decreasing=TRUE)]
+
+# indexing
+murder_rate <- (murders$total/murders$population)*100000
+index <- murder_rate<=1.00
+murders$state[index]
+sum(index)# true/false are coerced into 0/1
+
+index <- safe & west # using logical to query dfs
+index
+murders$state[index]
+
+# indexing functions
+which(index) # indices of true(alt of 'where')
+match(c("New York","Florida"),murders$state) # indices of first array elements
+c("Boston","Washington") %in% murders$state
+
+
+# data wrangling
+library(dplyr)
+# mutate, filter, select
+murders <- mutate(murders,rate=(total/population)*100000)
+murders$rate
+filter(murders,rate<0.71)
+select(murders,state,rate)
+#%>% pipe to combine functions
+murders %>% select(state,region,rate) %>% filter(rate<=0.7)
+
+# creating dataframes
+grades <- data.frame(names=c("John","Juan","Jean","Yao"),
+                     exam_1 = c(95,80,80,85),
+                     exam_2 = c(90,85,85,90),
+                     stringsAsFactors = FALSE)
+grades

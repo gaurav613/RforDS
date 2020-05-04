acca0c93014b81a088d4914080c194d672651dba
gaurav613 <gigs612@gmail.com>
2020-05-04 15:26:14 -0400

    addition rule, monty hall problem

--- probability.R
+++ probability.R
@@ -88,3 +88,34 @@ compute_prob <- function(B, n = 22){    # function to run Monte Carlo simulation
 
 prob <- sapply(B, compute_prob)    # apply compute_prob to many values of B
 plot(log10(B), prob, type = "l")    # plot a line graph of estimates 
+
+## addition rule - P(a or b) = P(a) + P(b) - P(a and b)
+
+## monty hall problem
+
+# sticking to original door
+B <- 10000
+stick <- replicate(B, {
+  doors <- as.character(1:3)
+  prize <- sample(c("car","goat","goat"))    # puts prizes in random order
+  prize_door <- doors[prize == "car"]    # note which door has prize
+  my_pick  <- sample(doors, 1)    # note which door is chosen
+  show <- sample(doors[!doors %in% c(my_pick, prize_door)],1)    # open door with no prize that isn't chosen
+  stick <- my_pick    # stick with original door
+  stick == prize_door    # test whether the original door has the prize
+})
+mean(stick)    # probability of choosing prize door when sticking = 1/3
+
+# switching to other door
+switch <- replicate(B, {
+  doors <- as.character(1:3)
+  prize <- sample(c("car","goat","goat"))    # puts prizes in random order
+  prize_door <- doors[prize == "car"]    # note which door has prize
+  my_pick  <- sample(doors, 1)    # note which door is chosen first
+  show <- sample(doors[!doors %in% c(my_pick, prize_door)], 1)    # open door with no prize that isn't chosen
+  switch <- doors[!doors%in%c(my_pick, show)]    # switch to the door that wasn't chosen first or opened
+  switch == prize_door    # test whether the switched door has the prize
+})
+mean(switch)    # probability of choosing prize door when switching = 2/3
+
+# it can be seen that prob. does not change to 1/2 once one of the doors is opened

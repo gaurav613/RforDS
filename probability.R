e9f8cc06be7fecd14cfdf3fdf4b70b22781191bb
gaurav613 <gigs612@gmail.com>
2020-05-05 19:19:41 -0400

    continuous probability

--- probability.R
+++ probability.R
@@ -119,3 +119,63 @@ switch <- replicate(B, {
 mean(switch)    # probability of choosing prize door when switching = 2/3
 
 # it can be seen that prob. does not change to 1/2 once one of the doors is opened
+
+## continuous probability
+
+# CDF - Cumulative Distribution Function
+library(tidyverse)
+library(dslabs)
+data(heights)
+x <- heights %>% filter(sex=="Male") %>% pull(height)
+
+F <- function(a) mean(x <= a)
+1 - F(70)    # probability of male taller than 70 inches using CDF
+
+# theoretical distribution
+
+# normal distribution using pnorm()
+1 - pnorm(70.5, mean(x), sd(x))
+
+# plot distribution of exact heights in data
+plot(prop.table(table(x)), xlab = "a = Height in inches", ylab = "Pr(x = a)")
+
+# probabilities in actual data over length 1 ranges containing an integer
+mean(x <= 68.5) - mean(x <= 67.5)
+mean(x <= 69.5) - mean(x <= 68.5)
+mean(x <= 70.5) - mean(x <= 69.5)
+
+# probabilities in normal approximation match well
+pnorm(68.5, mean(x), sd(x)) - pnorm(67.5, mean(x), sd(x))
+pnorm(69.5, mean(x), sd(x)) - pnorm(68.5, mean(x), sd(x))
+pnorm(70.5, mean(x), sd(x)) - pnorm(69.5, mean(x), sd(x))
+
+# probabilities in actual data over other ranges don't match normal approx as well
+mean(x <= 70.9) - mean(x <= 70.1)
+pnorm(70.9, mean(x), sd(x)) - pnorm(70.1, mean(x), sd(x))
+
+# probability density using dnorm(z, mu, sigma)
+# plotting probability density
+x <- seq(-4, 4, length = 100)
+data.frame(x, f = dnorm(x)) %>%
+  ggplot(aes(x, f)) +
+  geom_line()
+
+# monte carlo simulations for normally distributed variables
+
+n <- length(x)
+avg <- mean(x)
+s <- sd(x)
+simulated_heights <- rnorm(n,avg,s) # generate n random normal distribution with given avg and s
+
+data.frame(simulated_heights = simulated_heights) %>%
+  ggplot(aes(simulated_heights)) +
+  geom_histogram(color="black", binwidth = 2)
+
+# finding probability of the tallest person being over 7 feet tall
+B <- 10000
+tallest <- replicate(B, {
+  simulated_data <- rnorm(800, avg, s)    # generate 800 normally distributed random heights
+  max(simulated_data)    # determine the tallest height
+})
+mean(tallest >= 7*12)    # proportion of times that tallest person exceeded 7 feet (84 inches)
+

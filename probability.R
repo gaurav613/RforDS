178419b3487cf1a0d998922689c060ef73751d79
gaurav613 <gigs612@gmail.com>
2020-05-07 12:38:00 -0400

    interest rates, the big short

--- probability.R
+++ probability.R
@@ -210,3 +210,84 @@ S <- replicate(B, {
 mean(S < 0)    # probability of the casino losing money
 
 # The law of large numbers states that as  n  increases, the standard error of the average of a random variable decreases. In other words, when  n  is large, the average of the draws converges to the average of the urn.
+
+## the big short
+
+# interest rate sampling
+n <- 1000
+loss_per_foreclosure <- -200000
+p <- 0.02
+defaults <- sample( c(0,1), n, prob=c(1-p, p), replace = TRUE)
+sum(defaults * loss_per_foreclosure)
+
+# interest rate - monte carlo simulation
+B <- 10000
+losses <- replicate(B, {
+  defaults <- sample( c(0,1), n, prob=c(1-p, p), replace = TRUE) 
+  sum(defaults * loss_per_foreclosure)
+})
+
+n*(p*loss_per_foreclosure + (1-p)*0)    # expected value 
+sqrt(n)*abs(loss_per_foreclosure)*sqrt(p*(1-p))    # standard error
+
+# finding interest rate for expected value =0
+x = - loss_per_foreclosure*p/(1-p)
+x/180000 *100
+
+# calculating interest rate for 1% prob. of losing money
+l <- loss_per_foreclosure
+z <- qnorm(0.01)
+x <- -l*( n*p - z*sqrt(n*p*(1-p)))/ ( n*(1-p) + z*sqrt(n*p*(1-p)))
+x/180000    # interest rate
+loss_per_foreclosure*p + x*(1-p)    # expected value of the profit per loan
+n*(loss_per_foreclosure*p + x*(1-p)) # expected value of the profit over n loans
+
+# monte carlo simulation for the same
+B <- 100000
+profit <- replicate(B, {
+  draws <- sample( c(x, loss_per_foreclosure), n, 
+                   prob=c(1-p, p), replace = TRUE) 
+  sum(draws)
+})
+mean(profit)    # expected value of the profit over n loans
+mean(profit<0)    # probability of losing money
+
+# expected value with higher interest/default rate
+p <- .04
+loss_per_foreclosure <- -200000
+r <- 0.05
+x <- r*180000
+loss_per_foreclosure*p + x*(1-p)
+
+# number of loans for desired probability of losing money
+z <- qnorm(0.01)
+l <- loss_per_foreclosure
+n <- ceiling((z^2*(x-l)^2*p*(1-p))/(l*p + x*(1-p))^2)
+n    # number of loans required
+n*(loss_per_foreclosure*p + x * (1-p))    # expected profit over n loans
+
+# monte carlo simulation for the same
+
+# with known probability
+B <- 10000
+p <- 0.04
+x <- 0.05 * 180000
+profit <- replicate(B, {
+  draws <- sample( c(x, loss_per_foreclosure), n, 
+                   prob=c(1-p, p), replace = TRUE) 
+  sum(draws)
+})
+mean(profit)
+
+# with unknown probability
+p <- 0.04
+x <- 0.05*180000
+profit <- replicate(B, {
+  new_p <- 0.04 + sample(seq(-0.01, 0.01, length = 100), 1)
+  draws <- sample( c(x, loss_per_foreclosure), n, 
+                   prob=c(1-new_p, new_p), replace = TRUE)
+  sum(draws)
+})
+mean(profit)    # expected profit
+mean(profit < 0)    # probability of losing money
+mean(profit < -10000000)    # probability of losing over $10 million

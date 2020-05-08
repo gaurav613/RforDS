9b3056b2b56581e86f18d6be5ac05a66196cfc9c
gaurav613 <gigs612@gmail.com>
2020-05-08 14:50:12 -0400

    plotting cdfs, normal dists, quantiles/percentiles

---
+++ visualizations.R
@@ -0,0 +1,53 @@
+data(heights)
+head(heights)
+
+# make a table of category proportions
+prop.table(table(heights$sex))
+
+# plotting a cdf(heights data)
+my_data <- heights$height
+a <- seq(min(my_data), max(my_data), length = 100)    # define range of values spanning the dataset
+cdf_function <- function(x) {    # computes prob. for a single value
+  mean(my_data <= x)
+}
+cdf_values <- sapply(a, cdf_function)
+plot(a, cdf_values)
+
+# smooth density plots (- histogram with very small bins)
+# based in assumptions of analyst. The y-axis is scaled so that the area under the density curve sums to 1. This means that interpreting values on the y-axis is not straightforward. 
+
+# normal distributions
+# define x as vector of male heights
+index <- heights$sex=="Male"
+x <- heights$height[index]
+
+# calculate the mean and standard deviation manually
+average <- sum(x)/length(x)
+SD <- sqrt(sum((x - average)^2)/length(x))
+
+# built-in mean and sd functions - note that the audio and printed values disagree
+average <- mean(x)
+SD <- sd(x)
+c(average = average, SD = SD)
+
+# calculate standard units
+z <- scale(x)
+
+# quantiles & quantile plots
+# calculate observed and theoretical quantiles
+p <- seq(0.05, 0.95, 0.05)
+observed_quantiles <- quantile(x, p)
+theoretical_quantiles <- qnorm(p, mean = mean(x), sd = sd(x))
+
+# make QQ-plot
+plot(theoretical_quantiles, observed_quantiles)
+abline(0,1)
+
+# calculate proportion of values within 2 SD of mean
+mean(abs(z) < 2)
+
+# make QQ-plot with scaled values
+observed_quantiles <- quantile(z, p)
+theoretical_quantiles <- qnorm(p)
+plot(theoretical_quantiles, observed_quantiles)
+abline(0,1)

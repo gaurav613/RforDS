dc3526c17b20ba1c916f6efcad1a8fb988804e39
gaurav613 <gigs612@gmail.com>
2020-05-21 20:57:10 -0400

    confidence intervals and p-values

--- inference_and_modelling.R
+++ inference_and_modelling.R
@@ -98,3 +98,54 @@ data.frame(p = p, SE = SE) %>%
 # theory tells us that we would predict the election perfectly since the largest possible margin of error is around 0.3%
 # however, this may not be the case due to the possibility of a bias. Typically, bias appears to be about 1-2%
 
+## confidence intervals and p-values
+
+# confidence intervals
+
+# shaded areas in the curve demonstrate the confidence intervals
+data("nhtemp")
+data.frame(year = as.numeric(time(nhtemp)), temperature = as.numeric(nhtemp)) %>%
+  ggplot(aes(year, temperature)) +
+  geom_point() +
+  geom_smooth() +
+  ggtitle("Average Yearly Temperatures in New Haven")
+
+# monte carlo simulations for confidence intervals
+p <- 0.45
+N <- 1000
+X <- sample(c(0,1), size = N, replace = TRUE, prob = c(1-p, p))    # generate N observations
+X_hat <- mean(X)    # calculate X_hat
+SE_hat <- sqrt(X_hat*(1-X_hat)/N)    # calculate SE_hat, SE of the mean of N observations
+c(X_hat - 2*SE_hat, X_hat + 2*SE_hat)    # build interval of 2*SE above and below mean(95% interval)
+
+# finding Z(for some confidence interval q=99%) using qnorm()
+z <- qnorm(0.995)    # calculate z to solve for 99% confidence interval
+pnorm(qnorm(0.995))    # demonstrating that qnorm gives the z value for a given probability
+pnorm(qnorm(1-0.995))    # demonstrating symmetry of 1-qnorm
+pnorm(z) - pnorm(-z)    # demonstrating that this z value gives correct probability for interval
+
+# using monte carlo simulations to confirm 95% interval includes p 95% of the time
+B <- 10000
+inside <- replicate(B, {
+  X <- sample(c(0,1), size = N, replace = TRUE, prob = c(1-p, p))
+  X_hat <- mean(X)
+  SE_hat <- sqrt(X_hat*(1-X_hat)/N)
+  between(p, X_hat - 2*SE_hat, X_hat + 2*SE_hat)    # TRUE if p in confidence interval
+})
+mean(inside)
+
+# intervals are random, not p!
+
+# power - the probabiliyt of detecting an effect different from 0(eg. a tie in elections/p=0.5)
+N <- 25
+X_hat <- 0.48
+(2*X_hat - 1) + c(-2, 2)*2*sqrt(X_hat*(1-X_hat)/N)
+
+
+# p-values - probability of detecting an effect of certain size or larger when the null hypothesis is true
+# computing p-value for observed spread of 0.02
+N <- 100    # sample size
+z <- sqrt(N) * 0.02/0.5    # spread of 0.02
+1 - (pnorm(z) - pnorm(-z))
+
+#  To find p-values for a given z-score z in a normal distribution with mean mu and standard deviation sigma, use 2*(1-pnorm(z, mu, sigma)) instead.

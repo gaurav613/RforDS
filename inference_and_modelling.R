2b012bb5e46fac2425e4b62ee6eb378d2dbc5cff
gaurav613 <gigs612@gmail.com>
2020-05-24 14:18:18 -0400

    statistical models

--- inference_and_modelling.R
+++ inference_and_modelling.R
@@ -149,3 +149,101 @@ z <- sqrt(N) * 0.02/0.5    # spread of 0.02
 1 - (pnorm(z) - pnorm(-z))
 
 #  To find p-values for a given z-score z in a normal distribution with mean mu and standard deviation sigma, use 2*(1-pnorm(z, mu, sigma)) instead.
+
+## statistical models
+
+# poll aggregators
+
+# simulating polls
+d <- 0.039
+Ns <- c(1298, 533, 1342, 897, 774, 254, 812, 324, 1291, 1056, 2172, 516)
+p <- (d+1)/2
+
+# calculate confidence intervals of the spread
+confidence_intervals <- sapply(Ns, function(N){
+  X <- sample(c(0,1), size=N, replace=TRUE, prob = c(1-p, p))
+  X_hat <- mean(X)
+  SE_hat <- sqrt(X_hat*(1-X_hat)/N)
+  2*c(X_hat, X_hat - 2*SE_hat, X_hat + 2*SE_hat) - 1
+})
+
+# generate a data frame storing results
+polls <- data.frame(poll = 1:ncol(confidence_intervals),
+                    t(confidence_intervals), sample_size = Ns)
+names(polls) <- c("poll", "estimate", "low", "high", "sample_size")
+polls
+
+# calculate the spread of combined polls
+d_hat <- polls %>%
+  summarize(avg = sum(estimate*sample_size) / sum(sample_size)) %>%
+  .$avg
+
+p_hat <- (1+d_hat)/2
+moe <- 2*1.96*sqrt(p_hat*(1-p_hat)/sum(polls$sample_size)) # margin of error   
+round(d_hat*100,1)
+round(moe*100, 1)
+
+# pollsters and multileveled models
+# pollster bias - repeated polls given by a pollster have an expected value different from the actual spread
+# urn model doesn't account for pollster bias
+
+# generate simulated polling data
+data(polls_us_election_2016)
+names(polls_us_election_2016)
+
+# keep only national polls from week before election with a grade considered reliable
+polls <- polls_us_election_2016 %>%
+  filter(state == "U.S." & enddate >= "2016-10-31" &
+           (grade %in% c("A+", "A", "A-", "B+") | is.na(grade)))
+
+# add spread estimate
+polls <- polls %>%
+  mutate(spread = rawpoll_clinton/100 - rawpoll_trump/100)
+
+# compute estimated spread for combined polls
+d_hat <- polls %>%
+  summarize(d_hat = sum(spread * samplesize) / sum(samplesize)) %>%
+  .$d_hat
+
+# compute margin of error
+p_hat <- (d_hat+1)/2
+moe <- 1.96 * 2 * sqrt(p_hat*(1-p_hat)/sum(polls$samplesize))
+
+# histogram of the spread
+polls %>%
+  ggplot(aes(spread)) +
+  geom_histogram(color="black", binwidth = .01)
+
+# investing poll data and pollster bias
+# number of polls per pollster in week before election
+polls %>% group_by(pollster) %>% summarize(n())
+
+# plot results by pollsters with at least 6 polls
+polls %>% group_by(pollster) %>%
+  filter(n() >= 6) %>%
+  ggplot(aes(pollster, spread)) +
+  geom_point() +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1))
+
+# standard errors within each pollster
+polls %>% group_by(pollster) %>%
+  filter(n() >= 6) %>%
+  summarize(se = 2 * sqrt(p_hat * (1-p_hat) / median(samplesize)))
+
+# data-driven models
+# exepcted value = actual spread = d = 2p - 1
+
+# collect last result before the election for each pollster
+one_poll_per_pollster <- polls %>% group_by(pollster) %>%
+  filter(enddate == max(enddate)) %>%      # keep latest poll
+  ungroup()
+
+# histogram of spread estimates
+one_poll_per_pollster %>%
+  ggplot(aes(spread)) + geom_histogram(binwidth = 0.01)
+
+# construct 95% confidence interval
+results <- one_poll_per_pollster %>%
+  summarize(avg = mean(spread), se = sd(spread)/sqrt(length(spread))) %>%
+  mutate(start = avg - 1.96*se, end = avg + 1.96*se)
+round(results*100, 1)

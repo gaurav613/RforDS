408b09862abbfccfd224a8d02d6bc5156bcf94d0
gaurav613 <gigs612@gmail.com>
2020-05-28 19:47:39 -0400

    association testing

--- inference_and_modelling.R
+++ inference_and_modelling.R
@@ -477,3 +477,80 @@ one_poll_per_pollster %>%
 # quantile from t-distribution versus normal distribution
 qt(0.975, 14)    # 14 = nrow(one_poll_per_pollster) - 1
 qnorm(0.975)
+
+## association tests
+
+# research funding rates 
+# load and inspect research funding rates object
+library(tidyverse)
+library(dslabs)
+data(research_funding_rates)
+research_funding_rates
+
+# compute totals that were successful or not successful
+totals <- research_funding_rates %>%
+  select(-discipline) %>%
+  summarize_all(funs(sum)) %>%
+  summarize(yes_men = awards_men,
+            no_men = applications_men - awards_men,
+            yes_women = awards_women,
+            no_women = applications_women - awards_women)
+
+# compare percentage of men/women with awards
+totals %>% summarize(percent_men = yes_men/(yes_men + no_men),
+                     percent_women = yes_women/(yes_women + no_women))
+
+# two-by-two table for Lady Testing Tea example
+tab <- matrix(c(3,1,1,3), 2, 2)
+rownames(tab) <- c("Poured Before", "Poured After")
+colnames(tab) <- c("Guessed Before", "Guessed After")
+tab
+
+# p-value calculation with Fisher's Exact Test
+fisher.test(tab, alternative = "greater")
+
+# chi-squared test
+# The chi-squared test compares the observed two-by-two table to the two-by-two table expected by the null hypothesis and asks how likely it is that we see a deviation as large as observed or larger by chance.
+# The odds ratio states how many times larger the odds of an outcome are for one group relative to another group.
+
+# compute overall funding rate
+funding_rate <- totals %>%
+  summarize(percent_total = (yes_men + yes_women) / (yes_men + no_men + yes_women + no_women)) %>%
+  .$percent_total
+funding_rate
+
+# construct two-by-two table for observed data
+two_by_two <- tibble(awarded = c("no", "yes"),
+                     men = c(totals$no_men, totals$yes_men),
+                     women = c(totals$no_women, totals$yes_women))
+two_by_two
+
+# compute null hypothesis two-by-two table
+tibble(awarded = c("no", "yes"),
+       men = (totals$no_men + totals$yes_men) * c(1-funding_rate, funding_rate),
+       women = (totals$no_women + totals$yes_women) * c(1-funding_rate, funding_rate))
+
+# chi-squared test
+chisq_test <- two_by_two %>%
+  select(-awarded) %>%
+ chisq.test()
+chisq_test$p.value
+
+# odds ratio
+# odds of getting funding for men
+odds_men <- (two_by_two$men[2] / sum(two_by_two$men)) /
+  (two_by_two$men[1] / sum(two_by_two$men))
+
+# odds of getting funding for women
+odds_women <- (two_by_two$women[2] / sum(two_by_two$women)) /
+  (two_by_two$women[1] / sum(two_by_two$women))
+
+# odds ratio - how many times larger odds are for men than women
+odds_men/odds_women
+
+# sample size vs p-values and odd ratios
+# multiplying all observations by 10 decreases p-value without changing odds ratio
+two_by_two %>%
+  select(-awarded) %>%
+  mutate(men = men*10, women = women*10) %>%
+  chisq.test()

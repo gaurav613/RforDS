14988b0d0af74819cad49d790b0b1a6941888f7a
gaurav613 <gigs612@gmail.com>
2020-05-27 21:52:17 -0400

    forecasting, t-distributions

--- inference_and_modelling.R
+++ inference_and_modelling.R
@@ -265,3 +265,215 @@ test[outcome == "Disease"] <- sample(c("+", "-"), N_D, replace=TRUE, prob = c(ac
 test[outcome == "Healthy"] <- sample(c("-", "+"), N_H, replace=TRUE, prob = c(accuracy, 1-accuracy))
 
 table(outcome, test)
+
+# election forecasting
+
+# aggregating results by pollster
+library(tidyverse)
+library(dslabs)
+polls <- polls_us_election_2016 %>%
+  filter(state == "U.S." & enddate >= "2016-10-31" &
+           (grade %in% c("A+", "A", "A-", "B+") | is.na(grade))) %>%
+  mutate(spread = rawpoll_clinton/100 - rawpoll_trump/100)
+
+one_poll_per_pollster <- polls %>% group_by(pollster) %>%
+  filter(enddate == max(enddate)) %>%
+  ungroup()
+
+results <- one_poll_per_pollster %>%
+  summarize(avg = mean(spread), se = sd(spread)/sqrt(length(spread))) %>%
+  mutate(start = avg - 1.96*se, end = avg + 1.96*se)
+
+# posterior mean, standard error, credible interval, probability
+mu <- 0
+tau <- 0.035
+sigma <- results$se
+Y <- results$avg
+B <- sigma^2 / (sigma^2 + tau^2)
+posterior_mean <- B*mu + (1-B)*Y
+posterior_se <- sqrt(1 / (1/sigma^2 + 1/tau^2))
+
+posterior_mean
+posterior_se
+
+# 95% credible interval
+posterior_mean + c(-1.96, 1.96)*posterior_se
+
+# probability of d > 0
+1 - pnorm(0, posterior_mean, posterior_se)
+
+# mathematical representation of models
+
+# simulated data: Xj = d + sigma(j) <- random error for jth poll
+J <- 6
+N <- 2000
+d <- .021
+p <- (d+1)/2
+X <- d + rnorm(J, 0, 2*sqrt(p*(1-p)/N))
+
+# simulated data: Xi,j = d + sigma(i,j) <- random error for jth poll by ith pollster
+I <- 5
+J <- 6
+N <- 2000
+d <- .021
+p <- (d+1)/2
+X <- sapply(1:I, function(i){
+  d + rnorm(J, 0, 2*sqrt(p*(1-p)/N))
+})
+
+# simulated data: Xi,j = d + hi + sigma(i,j) <- house effect(pollster-to-pollster variability) + random error
+I <- 5
+J <- 6
+N <- 2000
+d <- .021
+p <- (d+1)/2
+h <- rnorm(I, 0, 0.025)    # assume standard error of pollster-to-pollster variability is 0.025
+X <- sapply(1:I, function(i){
+  d + rnorm(J, 0, 2*sqrt(p*(1-p)/N))
+})
+
+# prob. of d>0 with general bias = 0.025
+mu <- 0
+tau <- 0.035
+sigma <- sqrt(results$se^2 + .025^2)
+Y <- results$avg
+B <- sigma^2 / (sigma^2 + tau^2)
+
+posterior_mean <- B*mu + (1-B)*Y
+posterior_se <- sqrt(1 / (1/sigma^2 + 1/tau^2))
+
+1 - pnorm(0, posterior_mean, posterior_se) # 81.7%
+
+# predicting the electoral college
+# data of top-5 states ranked by electoral votes
+data("polls_us_election_2016")
+head(results_us_election_2016)
+
+results_us_election_2016 %>% arrange(desc(electoral_votes)) %>% top_n(5, electoral_votes)
+
+# getting avg and sd for each state
+results <- polls_us_election_2016 %>%
+  filter(state != "U.S." &
+           !grepl("CD", "state") &
+           enddate >= "2016-10-31" &
+           (grade %in% c("A+", "A", "A-", "B+") | is.na(grade))) %>%
+  mutate(spread = rawpoll_clinton/100 - rawpoll_trump/100) %>%
+  group_by(state) %>%
+  summarize(avg = mean(spread), sd = sd(spread), n = n()) %>%
+  mutate(state = as.character(state))
+
+# 10 closest races = battleground states
+results %>% arrange(abs(avg))
+
+# joining electoral college votes and results
+results <- left_join(results, results_us_election_2016, by="state")
+
+# states with no polls: note Rhode Island and District of Columbia = Democrat
+results_us_election_2016 %>% filter(!state %in% results$state)
+
+# assigns sd to states with just one poll as median of other sd values
+results <- results %>%
+  mutate(sd = ifelse(is.na(sd), median(results$sd, na.rm = TRUE), sd))
+
+# calculating posterior mean/sd
+mu <- 0
+tau <- 0.02
+results %>% mutate(sigma = sd/sqrt(n),
+                   B = sigma^2/ (sigma^2 + tau^2),
+                   posterior_mean = B*mu + (1-B)*avg,
+                   posterior_se = sqrt( 1 / (1/sigma^2 + 1/tau^2))) %>%
+  arrange(abs(posterior_mean))
+
+# monte carlo simulation of election night - without general bias
+mu <- 0
+tau <- 0.02
+clinton_EV <- replicate(1000, {
+  results %>% mutate(sigma = sd/sqrt(n),
+                     B = sigma^2/ (sigma^2 + tau^2),
+                     posterior_mean = B*mu + (1-B)*avg,
+                     posterior_se = sqrt( 1 / (1/sigma^2 + 1/tau^2)),
+                     simulated_result = rnorm(length(posterior_mean), posterior_mean, posterior_se),
+                     clinton = ifelse(simulated_result > 0, electoral_votes, 0)) %>%    # award votes if Clinton wins state
+    summarize(clinton = sum(clinton)) %>%    # total votes for Clinton
+    .$clinton + 7    # 7 votes for Rhode Island and DC
+})
+mean(clinton_EV > 269)    # over 269 votes wins election - 99.7% chance
+
+# histogram of outcomes
+data.frame(clinton_EV) %>%
+  ggplot(aes(clinton_EV)) +
+  geom_histogram(binwidth = 1) +
+  geom_vline(xintercept = 269)
+
+# monte carlo simulation for election night - with general bias
+mu <- 0
+tau <- 0.02
+bias_sd <- 0.03
+clinton_EV_2 <- replicate(1000, {
+  results %>% mutate(sigma = sqrt(sd^2/(n) + bias_sd^2),    # added bias_sd term
+                     B = sigma^2/ (sigma^2 + tau^2),
+                     posterior_mean = B*mu + (1-B)*avg,
+                     posterior_se = sqrt( 1 / (1/sigma^2 + 1/tau^2)),
+                     simulated_result = rnorm(length(posterior_mean), posterior_mean, posterior_se),
+                     clinton = ifelse(simulated_result > 0, electoral_votes, 0)) %>%    # award votes if Clinton wins state
+    summarize(clinton = sum(clinton)) %>%    # total votes for Clinton
+    .$clinton + 7    # 7 votes for Rhode Island and DC
+})
+mean(clinton_EV_2 > 269)    # over 269 votes wins election - 85.3% chance
+
+
+# forecasting
+# Pollsters also try to estimate  f(t) , the trend of  p  given time  t  using a model like:
+# Y(i,j,t)=d+b+hj+bt+f(t)+ϵi,j,t
+
+# variability across one pollster
+# select all national polls by one pollster
+one_pollster <- polls_us_election_2016 %>%
+  filter(pollster == "Ipsos" & state == "U.S.") %>%
+  mutate(spread = rawpoll_clinton/100 - rawpoll_trump/100)
+
+# the observed standard error is higher than theory predicts
+se <- one_pollster %>%
+  summarize(empirical = sd(spread),
+            theoretical = 2*sqrt(mean(spread)*(1-mean(spread))/min(samplesize)))
+se
+
+# the distribution of the data is not normal
+one_pollster %>% ggplot(aes(spread)) +
+  geom_histogram(binwidth = 0.01, color = "black")
+
+# trend across time for several pollsters
+polls_us_election_2016 %>%
+  filter(state == "U.S." & enddate >= "2016-07-01") %>%
+  group_by(pollster) %>%
+  filter(n() >= 10) %>%
+  ungroup() %>%
+  mutate(spread = rawpoll_clinton/100 - rawpoll_trump/100) %>%
+  ggplot(aes(enddate, spread)) +
+  geom_smooth(method = "loess", span = 0.1) +
+  geom_point(aes(color = pollster), show.legend = FALSE, alpha = 0.6)
+
+# plot raw percentages by time
+polls_us_election_2016 %>%
+  filter(state == "U.S." & enddate >= "2016-07-01") %>%
+  select(enddate, pollster, rawpoll_clinton, rawpoll_trump) %>%
+  rename(Clinton = rawpoll_clinton, Trump = rawpoll_trump) %>%
+  gather(candidate, percentage, -enddate, -pollster) %>%
+  mutate(candidate = factor(candidate, levels = c("Trump", "Clinton"))) %>%
+  group_by(pollster) %>%
+  filter(n() >= 10) %>%
+  ungroup() %>%
+  ggplot(aes(enddate, percentage, color = candidate)) +
+  geom_point(show.legend = FALSE, alpha = 0.4) +
+  geom_smooth(method = "loess", span = 0.15) +
+  scale_y_continuous(limits = c(30, 50))
+
+# calculating confidence interval with t-distribution
+z <- qt(0.975, nrow(one_poll_per_pollster) - 1)
+one_poll_per_pollster %>%
+  summarize(avg = mean(spread), moe = z*sd(spread)/sqrt(length(spread))) %>%
+  mutate(start = avg - moe, end = avg + moe)
+
+# quantile from t-distribution versus normal distribution
+qt(0.975, 14)    # 14 = nrow(one_poll_per_pollster) - 1
+qnorm(0.975)
